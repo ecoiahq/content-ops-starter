c4b5e45ca6dff946eccf9e6e0491bacafe6a0462
ecoiahq <ecoiahq@gmail.com>
2025-03-31 22:27:20 +0100

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,4 +1,14 @@
 // stackbit.config.ts
+import { defineStackbitConfig } from "@stackbit/types";
+
+export default defineStackbitConfig({
+  // ...
+  modelExtensions: [
+    // Extend the "Page" and "Post" models by defining them as page models
+    { name: "Page", type: "page" },
+    { name: "Post", type: "page" }
+  ]
+});
 import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
 
 export default defineStackbitConfig({
